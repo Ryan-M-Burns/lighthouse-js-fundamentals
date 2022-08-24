d110953db28cd848c82bde125ed708e39d9a9636
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-08-24 23:08:39 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -22,6 +22,7 @@ if (cold) {
 }
 */
 
+/*
 const temperature = 12;
 
 if (temperature < 0) {
@@ -33,3 +34,28 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+*/
+
+/*
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+*/
+
+/*
+const temperature = 51;
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+*/
+
+/*
+const raining = false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+*/
+
