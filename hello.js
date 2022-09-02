a9615959c5ee85398daea9c81d3772d1292125a1
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-02 18:29:58 +0000

    More function fun!

--- hello.js
+++ hello.js
@@ -3,7 +3,8 @@ const sayHello = function () {
   console.log("Hello, world");
 }
 sayHello();
-*/
+
+
 const sayHello = function (name) {
   console.log("Hello " + name);
 }
@@ -12,3 +13,18 @@ sayHello("Ryan");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+*/
+
+//Outputs "Hello John"
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+//Returns "Hello John" into a variable but doesn't print
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+//Call upon the variable to print "Hello John to console"
+console.log(greeting);
